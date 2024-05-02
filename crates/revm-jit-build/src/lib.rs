e088b5bf7632b3477249bb8960c5e906914035f2
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-05-02 16:13:04 +0200

    chore: simplify

--- crates/revm-jit-build/src/lib.rs
+++ crates/revm-jit-build/src/lib.rs
@@ -2,21 +2,12 @@
 #![cfg_attr(not(test), warn(unused_extern_crates))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
-use std::path::PathBuf;
-
-/// The linker dynamic list containing all the necessary symbols.
-pub const DYNAMIC_LIST: &str = include_str!("dynamic_list.txt");
-
 const MANGLE_PREFIX: &str = "__revm_jit_builtin_";
 
 /// Emits the linker flag to export all the necessary symbols.
 pub fn emit() {
-    if cfg!(target_vendor = "apple") {
-        println!("cargo:rustc-link-arg=-Wl,-exported_symbol,{MANGLE_PREFIX}*");
-    } else {
-        let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-        let dynamic_list = out_dir.join("dynamic_list.txt");
-        std::fs::write(&dynamic_list, DYNAMIC_LIST).unwrap();
-        println!("cargo:rustc-link-arg=-Wl,--dynamic-list={}", dynamic_list.display());
-    }
+    let target_vendor = std::env::var("CARGO_CFG_TARGET_VENDOR").unwrap();
+    let flag =
+        if target_vendor == "apple" { "-exported-symbol" } else { "--export-dynamic-symbol" };
+    println!("cargo:rustc-link-arg=-Wl,{flag},{MANGLE_PREFIX}*");
 }
