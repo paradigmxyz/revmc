5285bf49c88ac6ce54b08646bcd5c5c0c527088b
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-05-02 16:03:26 +0200

    fix: build script for macos

--- crates/revm-jit-build/src/lib.rs
+++ crates/revm-jit-build/src/lib.rs
@@ -7,10 +7,16 @@ use std::path::PathBuf;
 /// The linker dynamic list containing all the necessary symbols.
 pub const DYNAMIC_LIST: &str = include_str!("dynamic_list.txt");
 
+const MANGLE_PREFIX: &str = "__revm_jit_builtin_";
+
 /// Emits the linker flag to export all the necessary symbols.
 pub fn emit() {
-    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    let dynamic_list = out_dir.join("dynamic_list.txt");
-    std::fs::write(&dynamic_list, DYNAMIC_LIST).unwrap();
-    println!("cargo:rustc-link-arg=-Wl,--dynamic-list={}", dynamic_list.display());
+    if cfg!(target_vendor = "apple") {
+        println!("cargo:rustc-link-arg=-Wl,-exported_symbol,{MANGLE_PREFIX}*");
+    } else {
+        let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+        let dynamic_list = out_dir.join("dynamic_list.txt");
+        std::fs::write(&dynamic_list, DYNAMIC_LIST).unwrap();
+        println!("cargo:rustc-link-arg=-Wl,--dynamic-list={}", dynamic_list.display());
+    }
 }
