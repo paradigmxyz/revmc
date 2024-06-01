427dff69691afcc505837bc8f08d7a7e8830d1b0
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-06-01 20:48:15 +0200

    wip

--- .github/scripts/install_llvm_macos.sh
+++ .github/scripts/install_llvm_macos.sh
@@ -4,8 +4,10 @@ set -eo pipefail
 v=${1:-$LLVM_VERSION}
 
 brew install "llvm@${v}"
-prefix="$(brew --prefix llvm@${v})"
-echo "LLVM_SYS_${v}0_PREFIX=$prefix" >> $GITHUB_ENV
-echo "MLIR_SYS_${v}0_PREFIX=$prefix" >> $GITHUB_ENV
-echo "TABLEGEN_${v}0_PREFIX=$prefix" >> $GITHUB_ENV
-echo "$prefix/bin" >> $GITHUB_PATH
+prefix="$(brew --prefix "llvm@${v}")"
+{
+    echo "LLVM_SYS_${v}0_PREFIX=$prefix"
+    echo "MLIR_SYS_${v}0_PREFIX=$prefix"
+    echo "TABLEGEN_${v}0_PREFIX=$prefix"
+} >> "$GITHUB_ENV"
+echo "$prefix/bin" >> "$GITHUB_PATH"
--- .github/scripts/install_llvm_ubuntu.sh
+++ .github/scripts/install_llvm_ubuntu.sh
@@ -5,14 +5,24 @@ v=${1:-$LLVM_VERSION}
 bins=(clang llvm-config lld ld.lld FileCheck)
 llvm_sh=$(mktemp)
 
+echo "::group::Download LLVM $v"
 wget https://apt.llvm.org/llvm.sh -O "$llvm_sh"
+echo "::endgroup::"
+
 chmod +x "$llvm_sh"
+
+echo "::group::Install LLVM $v"
 "$llvm_sh" "$v" all
+echo "::endgroup::"
+
 for bin in "${bins[@]}"; do
     ln -fs "$(which "$bin-$v")" "/usr/bin/$bin"
 done
 
+echo "GITHUB_ENV=$GITHUB_ENV"
 prefix="$(llvm-config --prefix)"
-echo "LLVM_SYS_${v}0_PREFIX=$prefix" >> $GITHUB_ENV
-echo "MLIR_SYS_${v}0_PREFIX=$prefix" >> $GITHUB_ENV
-echo "TABLEGEN_${v}0_PREFIX=$prefix" >> $GITHUB_ENV
+{
+    echo "LLVM_SYS_${v}0_PREFIX=$prefix"
+    echo "MLIR_SYS_${v}0_PREFIX=$prefix"
+    echo "TABLEGEN_${v}0_PREFIX=$prefix"
+} >> "$GITHUB_ENV"
