61c574375ba3167668e01c0ea27aacdabcf78062
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-06-28 17:03:54 +0200

    fix

--- examples/runner/src/main.rs
+++ examples/runner/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let num =
         std::env::args().nth(1).map(|s| s.parse().unwrap()).unwrap_or_else(|| U256::from(100));
     // The bytecode runs fib(input + 1), so we need to subtract 1.
-    let actual_num = num.wrapping_sub(U256::from(1));
+    let actual_num = num.saturating_sub(U256::from(1));
 
     let db = CacheDB::new(EmptyDB::new());
     let mut evm = build_evm(db);
