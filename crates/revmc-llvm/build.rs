bbb4b272152e4751456e906420a433224506e2ed
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2026-04-12 11:40:37 +0200

    chore: tweak build script

--- crates/revmc-llvm/build.rs
+++ crates/revmc-llvm/build.rs
@@ -3,9 +3,22 @@
 fn main() {
     println!("cargo:rerun-if-changed=cpp/lib.cpp");
 
-    let cxxflags =
-        std::process::Command::new("llvm-config").arg("--cxxflags").output().expect("llvm-config");
-    let cxxflags = String::from_utf8(cxxflags.stdout).unwrap();
+    for (k, v) in std::env::vars() {
+        eprintln!("{k}={v}");
+    }
+
+    let cxxflags = ["llvm-config", "llvm-config-22"]
+        .into_iter()
+        .find_map(|llvm_config| {
+            match std::process::Command::new(llvm_config).arg("--cxxflags").output() {
+                Ok(output) => Some(String::from_utf8(output.stdout).unwrap()),
+                Err(e) => {
+                    eprintln!("failed to run {llvm_config}: {e}");
+                    None
+                }
+            }
+        })
+        .expect("no llvm-config found");
 
     cc::Build::new()
         .cpp(true)
